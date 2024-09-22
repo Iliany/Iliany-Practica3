a09ec03e4f8db5414b8fc759733d97dfa6cbd5f9
Iliany Roblero Roblero <158374976+Iliany@users.noreply.github.com>
2024-09-22 10:00:09 -0600

    Update conexionBD.php

--- conexionBD.php
+++ conexionBD.php
@@ -8,4 +8,11 @@ $dbname = "inventario";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+} else {
+ 
+}
+?>
+
 
